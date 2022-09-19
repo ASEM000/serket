06c04b717ccbe16bde9d4ea4780a7ff7170eb5b3
ASEM000 <mahmoudasem00@gmail.com>
2022-09-19 14:48:31 +0900

    version bump

--- serket/__init__.py
+++ serket/__init__.py
@@ -1,9 +1,9 @@
 from kernex import kmap, kscan
-from pytreeclass import static_field, treeclass
+from pytreeclass import nondiff_field, treeclass
 
 from . import nn
 from .operators import diff, diff_and_grad
 
-__all__ = ("nn", "treeclass, static_field", "kmap", "kscan", "diff", "diff_and_grad")
+__all__ = ("nn", "treeclass, nondiff_field", "kmap", "kscan", "diff", "diff_and_grad")
 
 __version__ = "0.0.3"
