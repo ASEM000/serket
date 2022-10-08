e4a6d780ffbc1383dd725aa46f3cd1f9a58a521d
ASEM000 <mahmoudasem00@gmail.com>
2022-10-09 07:25:06 +0900

    Update test_sequential.py

--- tests/test_sequential.py
+++ tests/test_sequential.py
@@ -9,7 +9,7 @@ def test_sequential():
     model = Sequential([Lambda(lambda x: x + 1), Lambda(lambda x: x + 1)])
     assert model(1.0) == 3.0
 
-    model = Sequential([lambda x: x])
+    model = Sequential([lambda x, key: x])
     assert model(1.0) == 1.0
 
 
