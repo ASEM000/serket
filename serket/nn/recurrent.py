99bf79fef75d0afa9d68aa4c539a9d3dbf01f2e9
ASEM000 <mahmoudasem00@gmail.com>
2022-10-09 07:11:37 +0900

    Update recurrent.py

--- serket/nn/recurrent.py
+++ serket/nn/recurrent.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
+from typing import Callable
+
 import jax
 import jax.numpy as jnp
 import jax.random as jr
 import pytreeclass as pytc
+from jax.nn import sigmoid as σ
 
 from serket.nn import Linear
+
+
+@pytc.treeclass
+class RNNState:
+    hidden_state: jnp.ndarray
 
 
 @pytc.treeclass
@@ -16,15 +24,24 @@ class RNNCell:
     in_and_hidden_to_hidden: Linear
 
     def __init__(
-        self, in_features: int, hidden_features: int, *, key: jr.PRNGKey = jr.PRNGKey(0)
+        self,
+        in_features: int,
+        hidden_features: int,
+        *,
+        weight_init_func: Callable = jax.nn.initializers.he_normal(),
+        bias_init_func: Callable | None = lambda key, shape: jnp.zeros(shape),
+        key: jr.PRNGKey = jr.PRNGKey(0),
     ):
-        """Vanilla RNN
-        see https://pytorch.org/docs/stable/generated/torch.nn.RNN.html
+        """Vanilla RNN cell that defines the update rule for the hidden state
+        See:
+            https://pytorch.org/docs/stable/generated/torch.nn.RNN.html
 
         Args:
             in_features (int): input features
             hidden_features (int): hidden features
-            key (jr.PRNGKey, optional): random key. Defaults to jr.PRNGKey(0).
+            weight_init_func (Callable, optional): . Defaults to jax.nn.initializers.he_normal().
+            bias_init_func (Callable, optional): . Defaults to lambdakey.
+            key (jr.PRNGKey, optional):  . Defaults to jr.PRNGKey(0).
         """
 
         k1, k2 = jr.split(key, 2)
@@ -37,17 +54,23 @@ class RNNCell:
         )
 
     def __call__(
-        self, x: jnp.ndarray, h: jnp.ndarray, **kwargs
+        self, x: jnp.ndarray, state: RNNState, **kwargs
     ) -> tuple[jnp.ndarray, jnp.ndarray]:
+        h = state.hidden_state
         h = jnp.tanh(self.in_and_hidden_to_hidden(jnp.concatenate([x, h], axis=-1)))
         return h, h
+
+
+@pytc.treeclass
+class LSTMState:
+    hidden_state: jnp.ndarray
+    cell_state: jnp.ndarray
 
 
 @pytc.treeclass
 class LSTMCell:
     in_features: int = pytc.nondiff_field()
     hidden_features: int = pytc.nondiff_field()
-    forget_bias: float
     in_and_hidden_to_hidden: Linear
 
     def __init__(
@@ -55,30 +78,42 @@ class LSTMCell:
         in_features: int,
         hidden_features: int,
         *,
-        layer_norm: bool,
-        forget_bias: float = 0.0,
+        weight_init_func: Callable = jax.nn.initializers.he_normal(),
+        bias_init_func: Callable | None = lambda key, shape: jnp.zeros(shape),
         key: jr.PRNGKey = jr.PRNGKey(0),
     ):
+        """Defines the update rule for the hidden state and the cell state
 
+        See:
+            https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
+            https://github.com/deepmind/dm-haiku/blob/main/haiku/_src/recurrent.py
+
+        Args:
+            in_features (int): input features
+            hidden_features (int): hidden features
+            weight_init_func (Callable, optional): . Defaults to jax.nn.initializers.he_normal().
+            bias_init_func (Callable, optional): . Defaults to lambdakey.
+            key (jr.PRNGKey, optional):  . Defaults to jr.PRNGKey(0).
+        """
         self.hidden_features = hidden_features
         self.in_features = in_features
-        self.forget_bias = forget_bias
 
         self.in_and_hidden_to_hidden = Linear(
             in_features=in_features + hidden_features,
-            out_features=hidden_features,
-            bias_init_func=None,
+            out_features=4 * hidden_features,
+            weight_init_func=weight_init_func,
+            bias_init_func=bias_init_func,
             key=key,
         )
 
     def __call__(
-        self, x: jnp.ndarray, h_c: jnp.ndarray, **kwargs
+        self, x: jnp.ndarray, state: LSTMState, **kwargs
     ) -> tuple[tuple[jnp.ndarray, jnp.ndarray], jnp.ndarray]:
 
-        h, c = h_c
+        h, c = state.hidden_state, state.cell_state
         ifgo = self.in_and_hidden_to_hidden(jnp.concatenate([x, h], axis=-1))
         i, f, g, o = jnp.split(ifgo, 4, axis=-1)
-        f = jax.nn.sigmoid(f + self.forget_bias)
-        c = f * c + jax.nn.sigmoid(i) + jnp.tanh(g)
-        h = jax.nn.sigmoid(o) * jnp.tanh(c)
-        return (h, c), h
+        i, f, g, o = σ(i), σ(f), jnp.tanh(g), σ(o)
+        c = f * c + i * g
+        h = o * jnp.tanh(c)
+        return LSTMState(h, c)
