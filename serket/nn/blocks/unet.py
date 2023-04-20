2c7ca83b39e1ea4bb0fe651b1694c710f199e4e5
ASEM000 <mahmoudasem00@gmail.com>
2023-04-20 09:50:56 +0900

    block test

--- serket/nn/blocks/unet.py
+++ serket/nn/blocks/unet.py
@@ -9,6 +9,7 @@ import jax.numpy as jnp
 import pytreeclass as pytc
 
 import serket as sk
+from serket.nn.callbacks import positive_int_cb
 
 
 class ResizeAndCat(pytc.TreeClass):
@@ -36,7 +37,7 @@ class DoubleConvBlock(pytc.TreeClass):
             bias_init_func=None,
         )
 
-    def __call__(self, x: jax.Array, **kwargs) -> jax.Array:
+    def __call__(self, x: jax.Array, **k) -> jax.Array:
         x = self.conv1(x)
         x = jax.nn.relu(x)
         x = self.conv2(x)
@@ -47,38 +48,34 @@ class DoubleConvBlock(pytc.TreeClass):
 class UpscaleBlock(pytc.TreeClass):
     def __init__(self, in_features: int, out_features: int):
         self.conv = sk.nn.Conv2DTranspose(
-            in_features=in_features, out_features=out_features, kernel_size=2, strides=2
+            in_features=in_features,
+            out_features=out_features,
+            kernel_size=2,
+            strides=2,
         )
 
-    def __call__(self, x: jax.Array, **kwargs) -> jax.Array:
-        # x = self.upscale(x)
-        x = self.conv(x)
-        return x
+    def __call__(self, x: jax.Array, **k) -> jax.Array:
+        return self.conv(x)
 
 
 class UNetBlock(pytc.TreeClass):
-    in_features: int = pytc.field(callbacks=[pytc.freeze])
-    out_features: int = pytc.field(callbacks=[pytc.freeze])
-    blocks: int = pytc.field(callbacks=[pytc.freeze])
-    init_features: int = pytc.field(callbacks=[pytc.freeze])
+    """Vanilla UNet
 
-    def __init__(
-        self,
-        in_features: int,
-        out_features: int,
-        blocks: int = 4,
-        init_features: int = 64,
-    ):
-        """Vanilla UNet
+    Args:
+        in_features : number of input channels. This is the number of channels in the input image.
+        out_features : number of output channels. This is the number of classes
+        blocks : number of blocks in the UNet architecture . Default is 4
+        init_features : number of features in the first block. Default is 64
+    """
 
-        Args:
-            in_features : number of input channels. This is the number of channels in the input image.
-            out_features : number of output channels. This is the number of classes
-            blocks : number of blocks in the UNet architecture . Default is 4
-            init_features : number of features in the first block. Default is 64
+    in_features: int = pytc.field(callbacks=[positive_int_cb])
+    out_features: int = pytc.field(callbacks=[positive_int_cb])
+    blocks: int = pytc.field(callbacks=[positive_int_cb], default=4)
+    init_features: int = pytc.field(callbacks=[positive_int_cb], default=64)
 
-
-        Summary:
+    def __post_init__(self):
+        """
+        Note:
             d0_1 :
                 block_number = 0 , operation = (conv->relu) x2
             d0_2 :
@@ -98,85 +95,41 @@ class UNetBlock(pytc.TreeClass):
             f0_1 :
                 final output layer
 
-        Two block example:
-            >>> print(sk.nn.UNetBlock(3,1,blocks=2, init_features=32).summary(show_config=False))
-            ┌──────────┬────────────────────────────┬──────────┬───────────────┐
-            │Name      │Type                        │Param #   │Size           │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │d0_1/conv1│DoubleConvBlock/Conv2D      │864(0)    │3.38KB(0.00B)  │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │d0_1/conv2│DoubleConvBlock/Conv2D      │9,216(0)  │36.00KB(0.00B) │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │d0_2      │MaxPool2D                   │0(0)      │0.00B(0.00B)   │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │d1_1/conv1│DoubleConvBlock/Conv2D      │18,432(0) │72.00KB(0.00B) │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │d1_1/conv2│DoubleConvBlock/Conv2D      │36,864(0) │144.00KB(0.00B)│
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │d1_2      │MaxPool2D                   │0(0)      │0.00B(0.00B)   │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │b0_1/conv1│DoubleConvBlock/Conv2D      │73,728(0) │288.00KB(0.00B)│
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │b0_1/conv2│DoubleConvBlock/Conv2D      │147,456(0)│576.00KB(0.00B)│
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u1_1/conv │UpscaleBlock/Conv2DTranspose│32,832(0) │128.25KB(0.00B)│
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u1_2      │ResizeAndCat                │0(0)      │0.00B(0.00B)   │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u1_3/conv1│DoubleConvBlock/Conv2D      │73,728(0) │288.00KB(0.00B)│
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u1_3/conv2│DoubleConvBlock/Conv2D      │36,864(0) │144.00KB(0.00B)│
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u0_1/conv │UpscaleBlock/Conv2DTranspose│8,224(0)  │32.12KB(0.00B) │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u0_2      │ResizeAndCat                │0(0)      │0.00B(0.00B)   │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u0_3/conv1│DoubleConvBlock/Conv2D      │18,432(0) │72.00KB(0.00B) │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │u0_3/conv2│DoubleConvBlock/Conv2D      │9,216(0)  │36.00KB(0.00B) │
-            ├──────────┼────────────────────────────┼──────────┼───────────────┤
-            │f0_1      │Conv2D                      │33(0)     │132.00B(0.00B) │
-            └──────────┴────────────────────────────┴──────────┴───────────────┘
-            Total count :	465,889(0)
-            Dynamic count :	465,889(0)
-            Frozen count :	0(0)
-            --------------------------------------------------------------------
-            Total size :	1.78MB(0.00B)
-            Dynamic size :	1.78MB(0.00B)
-            Frozen size :	0.00B(0.00B)
-            ====================================================================
         """
-
-        self.in_features = in_features
-        self.out_features = out_features
-        self.blocks = blocks
-        self.init_features = init_features
-
-        self.d0_1 = DoubleConvBlock(in_features, init_features)
+        self.d0_1 = DoubleConvBlock(self.in_features, self.init_features)
         self.d0_2 = sk.nn.MaxPool2D(kernel_size=2, strides=2)
 
-        for i in range(1, blocks):
-            # conv->relu x2
-            layer = DoubleConvBlock(init_features * (2 ** (i - 1)), init_features * (2**i))  # fmt: skip
+        for i in range(1, self.blocks):
+            in_dim = self.init_features * (2 ** (i - 1))
+            out_dim = self.init_features * (2**i)
+
+            layer = DoubleConvBlock(in_dim, out_dim)
             setattr(self, f"d{i}_1", layer)
             setattr(self, f"d{i}_2", sk.nn.MaxPool2D(kernel_size=2, strides=2))
 
-        self.b0_1 = DoubleConvBlock(init_features * (2 ** (blocks - 1)), init_features * (2 ** (blocks)))  # fmt: skip
+        self.b0_1 = DoubleConvBlock(
+            self.init_features * (2 ** (self.blocks - 1)),
+            self.init_features * (2 ** (self.blocks)),
+        )
 
-        for i in range(blocks, 0, -1):
+        for i in range(self.blocks, 0, -1):
             # upscale and conv to halves channels size and double row,col size
-            layer = UpscaleBlock(init_features * (2 ** (i)), init_features * (2 ** (i - 1)))  # fmt: skip
+            in_dim = self.init_features * (2 ** (i - 1))
+            out_dim = self.init_features * (2**i)
+
+            layer = UpscaleBlock(out_dim, in_dim)
             setattr(self, f"u{i-1}_1", layer)
 
             layer = ResizeAndCat()
             setattr(self, f"u{i-1}_2", layer)
 
-            layer = DoubleConvBlock(init_features * (2 ** (i)), init_features * (2 ** (i - 1)))  # fmt: skip
+            layer = DoubleConvBlock(out_dim, in_dim)
             setattr(self, f"u{i-1}_3", layer)
 
-        self.f0_1 = sk.nn.Conv2D(init_features, out_features, kernel_size=1)
+        self.f0_1 = sk.nn.Conv2D(self.init_features, self.out_features, kernel_size=1)
 
-    def __call__(self, x: jax.Array) -> jax.Array:
+    def __call__(self, x: jax.Array, **k) -> jax.Array:
+        # TODO: fix to not record intermediate results
         result = dict()
         blocks = self.blocks
 
@@ -192,12 +145,18 @@ class UNetBlock(pytc.TreeClass):
 
         result[f"u{blocks-1}_1"] = getattr(self, f"u{blocks-1}_1")(result["b0_1"])
         lhs_key, rhs_key = f"u{blocks-1}_1", f"d{blocks-1}_1"
-        result[f"u{blocks-1}_2"] = getattr(self, f"u{blocks-1}_2")(result[lhs_key], result[rhs_key])  # fmt: skip
-        result[f"u{blocks-1}_3"] = getattr(self, f"u{blocks-1}_3")(result[f"u{blocks-1}_2"])  # fmt: skip
+        result[f"u{blocks-1}_2"] = getattr(self, f"u{blocks-1}_2")(
+            result[lhs_key], result[rhs_key]
+        )
+        result[f"u{blocks-1}_3"] = getattr(self, f"u{blocks-1}_3")(
+            result[f"u{blocks-1}_2"]
+        )
 
         for i in range(blocks - 1, 0, -1):
             result[f"u{i-1}_1"] = getattr(self, f"u{i-1}_1")(result[f"u{i}_3"])
-            result[f"u{i-1}_2"] = getattr(self, f"u{blocks-1}_2")(result[f"u{i-1}_1"], result[f"d{i-1}_1"])  # fmt: skip
+            result[f"u{i-1}_2"] = getattr(self, f"u{blocks-1}_2")(
+                result[f"u{i-1}_1"], result[f"d{i-1}_1"]
+            )
             result[f"u{i-1}_3"] = getattr(self, f"u{i-1}_3")(result[f"u{i-1}_2"])
 
         return self.f0_1(result["u0_3"])
--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,29 +1,38 @@
 from __future__ import annotations
 
-# from serket.nn.blocks import UNetBlock, VGG16Block, VGG19Block
+import jax
+import jax.numpy as jnp
 
-# def test_vgg16_block():
-#     block = VGG16Block(3)
-#     count, _ = _reduce_count_and_size(block)
-#     assert count.real == 14_714_688
-#     model = VGG16Block(3)
-#     assert model(jnp.ones([3, 224, 224])).shape == (512, 1, 1)
+from serket.nn.blocks import UNetBlock, VGG16Block, VGG19Block
 
 
-# def test_vgg19_block():
-#     block = VGG19Block(3)
-#     count, _ = _reduce_count_and_size(block)
-#     assert count.real == 20_024_384
+def count_parameters(model):
+    is_array = lambda x: isinstance(x, jax.Array)
+    count = 0
 
-#     model = VGG19Block(3)
-#     assert model(jnp.ones([3, 224, 224])).shape == (512, 1, 1)
+    def map_func(leaf):
+        nonlocal count
+        if is_array(leaf):
+            count += leaf.size
+        return leaf
+
+    jax.tree_map(map_func, model)
+    return count
 
 
-# def test_unet_block():
-#     block = UNetBlock(3, 1, init_features=32)
-#     count, _ = _reduce_count_and_size(block)
-#     assert count.real == 7_757_153
+def test_vgg16_block():
+    model = VGG16Block(3)
+    assert count_parameters(model) == 14_714_688
+    assert model(jnp.ones([3, 224, 224])).shape == (512, 1, 1)
 
-#     model = UNetBlock(3, 1, 2)
 
-#     assert model(jnp.ones((3, 320, 320))).shape == (1, 320, 320)
+def test_vgg19_block():
+    model = VGG19Block(3)
+    assert count_parameters(model) == 20_024_384
+    assert model(jnp.ones([3, 224, 224])).shape == (512, 1, 1)
+
+
+def test_unet_block():
+    # assert count_parameters(UNetBlock(3, 1, 32)) == 7_757_153
+    model = UNetBlock(3, 1, 2)
+    assert model(jnp.ones((3, 320, 320))).shape == (1, 320, 320)
