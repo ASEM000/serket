e387c7683d0699289d4ea93d3dc297e1f38eebde
ASEM000 <mahmoudasem00@gmail.com>
2023-06-07 00:36:31 +0900

    Update initialization.py

--- serket/nn/initialization.py
+++ serket/nn/initialization.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import annotations
 
 from types import FunctionType
 from typing import Any, Callable, Literal, Union, get_args
