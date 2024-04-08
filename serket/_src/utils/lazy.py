e300a87e6e6f0178c1fe4fba6b16b7a533655773
ASEM000 <mahmoudasem00@gmail.com>
2024-04-08 21:55:18 +0900

    refactor lazy

--- serket/_src/utils/lazy.py
+++ serket/_src/utils/lazy.py
@@ -28,6 +28,31 @@ P = ParamSpec("P")
 T = TypeVar("T")
 
 
+def handle_pos_or_kw(param: inspect.Parameter, index: int, args, kwargs):
+    if len(args) > 0:
+        return args[index]
+    if param.name in kwargs:
+        return kwargs[param.name]
+    if param.default is not inspect.Parameter.empty:
+        return param.default
+    raise TypeError(f"{param.name} is required")
+
+
+def handle_kw_only(param: inspect.Parameter, index, args, kwargs):
+    del index, args
+    if param.name in kwargs:
+        return kwargs[param.name]
+    if param.default is not inspect.Parameter.empty:
+        return param.default
+    raise TypeError(f"{param.name} is required")
+
+
+ParamHandler = Callable[[inspect.Parameter, int, tuple[Any, ...], dict[str, Any]], Any]
+rules: dict[Any, ParamHandler] = {}
+rules[inspect.Parameter.POSITIONAL_OR_KEYWORD] = handle_pos_or_kw
+rules[inspect.Parameter.KEYWORD_ONLY] = handle_kw_only
+
+
 def maybe_lazy_init(
     func: Callable[P, T],
     is_lazy: Callable[..., bool],
@@ -50,72 +75,36 @@ def maybe_lazy_init(
         The decorated ``__init__`` method.
     """
 
+    @ft.wraps(func)
     def inner(instance, *a, **k):
         if not is_lazy(instance, *a, **k):
-            # continue with the original initialization
             return func(instance, *a, **k)
 
         # store the input arguments to the instance
         # until the instance is re-initialized (materialized)
         # then use the stored arguments to re-initialize the instance
-        kwargs: dict[str, Any] = dict()
+        for i, p in enumerate(get_params(func)[1:]):
+            setattr(instance, p.name, rules[p.kind](p, i, a, k))
 
-        for index, param in enumerate(get_params(func)[1:]):
-            # skip the self argument
-            if param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD:
-                # fetch from the positional arguments
-                # or the keyword arguments or the default value if exists
-                if len(a) > index:
-                    # fetch from the positional arguments if available
-                    kwargs[param.name] = a[index]
-                elif param.name in k:
-                    # fetch from the keyword arguments
-                    kwargs[param.name] = k[param.name]
-                elif param.default is not inspect.Parameter.empty:
-                    # fetch from the default value if exists
-                    kwargs[param.name] = param.default
-
-            elif param.kind is inspect.Parameter.KEYWORD_ONLY:
-                # fetch from the keyword arguments
-                # or the default value
-                if param.name in k:
-                    # fetch from the keyword arguments if exists
-                    kwargs[param.name] = k[param.name]
-                elif param.default is not inspect.Parameter.empty:
-                    # fetch from the default value if exists
-                    kwargs[param.name] = param.default
-            else:
-                # dont support positional only arguments, etc.
-                # not to complicate things
-                raise NotImplementedError(f"{param.kind=}")
-
-        for key, value in kwargs.items():
-            # set the attribute to the instance
-            # these will be reused to re-initialize the instance
-            # after the first call
-            setattr(instance, key, value)
-
-        # halt the initialization of the instance
-        # and move to the next call
         return None
 
-    return ft.wraps(func)(inner)
+    return inner
 
 
-LAZY_CALL_ERROR = """\
-Cannot call ``{func_name}`` directly on a lazy layer.
-use ``value_and_tree(lambda layer: layer{func_name}(...))(layer)`` instead to return a tuple of:
+LAZY_ERROR = """\
+Cannot call ``{fname}`` directly on a lazy layer.
+use ``value_and_tree(lambda layer: layer{fname}(...))(layer)`` instead to return a tuple of:
     - Layer output.
     - Materialized layer.
 
 Example:
-    >>> layer = {class_name}(...)
+    >>> layer = {cname}(...)
     >>> layer(input)  # this will raise an error
     ...
     
     Instead use the following pattern:
 
-    >>> output, material = value_and_tree(lambda layer: layer{func_name}(input))(layer)
+    >>> output, material = value_and_tree(lambda layer: layer{fname}(input))(layer)
     >>> material(input)
     ...
 """
@@ -148,29 +137,28 @@ def maybe_lazy_call(
 
         # the instance variables are the input arguments
         # to the ``__init__`` method
-        kwargs = dict(vars(instance))
+        partial_mapping = dict(vars(instance))
 
         for key, update in updates.items():
-            kwargs[key] = update(instance, *a, **k)
+            partial_mapping[key] = update(instance, *a, **k)
 
         try:
-            for key in kwargs:
+            for key in partial_mapping:
                 # clear the instance information (i.e. the initial input arguments)
                 # use ``delattr`` to raise an error if the instance is immutable
                 # which is marking the instance as lazy and immutable
                 delattr(instance, key)
         except AttributeError:
             # the instance is lazy and immutable
-            func_name = func.__name__
-            func_name = "" if func_name == "__call__" else f".{func_name}"
-            class_name = type(instance).__name__
-            kwargs = dict(func_name=func_name, class_name=class_name)
-            raise RuntimeError(LAZY_CALL_ERROR.format(**kwargs))
+            fname = "" if (fname := func.__name__) == "__call__" else f".{fname}"
+            cname = type(instance).__name__
+            raise RuntimeError(LAZY_ERROR.format(fname=fname, cname=cname))
 
         # re-initialize the instance with the resolved arguments
         # this will only works under `value_and_tree` that allows
         # the instance to be mutable with it's context after being copied first
-        type(instance).__init__(instance, **kwargs)
+        init = getattr(type(instance), "__init__")
+        init(instance, **partial_mapping)
         # call the decorated function
         return func(instance, *a, **k)
 
